6932ba2286a62bd1a0425baebe053681a36ef8b1
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2022-10-27 13:06:05 -0300

    chore(normalize-string): separate upper to lower case

--- functions/lib/integration/helpers/normalize-string.js
+++ functions/lib/integration/helpers/normalize-string.js
@@ -1,10 +1,16 @@
 module.exports = str => {
-    return str?.replace(/áàãâÁÀÃÂ/g, 'a')
-      ?.replace(/éêÉÊ/g, 'e')
-      ?.replace(/óõôÓÕÔ/g, 'o')
-      ?.replace(/íÍ/g, 'i')
-      ?.replace(/úÚ/g, 'u')
-      ?.replace(/çÇ/g, 'c')
+    return str?.replace(/áàãâ/g, 'a')
+      ?.replace(/ÁÀÃÂ/g, 'A')
+      ?.replace(/éê/g, 'e')
+      ?.replace(/ÉÊ/g, 'E')
+      ?.replace(/óõô/g, 'o')
+      ?.replace(/ÓÕÔ/g, 'O')
+      ?.replace(/í/g, 'i')
+      ?.replace(/Í/g, 'I')
+      ?.replace(/ú/g, 'u')
+      ?.replace(/Ú/g, 'U')
+      ?.replace(/ç/g, 'c')
+      ?.replace(/Ç/g, 'C')
       ?.replace('&', 'e')
 }
   
