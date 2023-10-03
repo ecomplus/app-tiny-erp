e79ab9853cfa0ae51201225454b9c460af96f26e
Leonardo Matos <leomp120894@gmail.com>
2023-10-03 16:05:17 -0300

    fix: listing less orders on check exports cron filtering any _from-tiny_ fulfillment

--- functions/index.js
+++ functions/index.js
@@ -171,7 +171,7 @@ exports.onTinyEvents = require('./lib/pubsub/create-topic')
 
 const checkExportedOrders = require('./lib/integration/check-exported-orders')
 exports.checkExportedOrders = functions.runWith({ timeoutSeconds: 300 })
-  .pubsub.schedule('26 * * * *').onRun(() => {
+  .pubsub.schedule('12 * * * *').onRun(() => {
     return prepareAppSdk().then(appSdk => {
       checkExportedOrders({ appSdk })
     })
